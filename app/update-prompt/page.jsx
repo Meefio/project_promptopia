80696494e349b786c08643e1df0d93b96ac8ef95
Michal Rowinski <mic.rowinski@gmail.com>
2024-08-24 14:16:18 +0200

    added Suspense fallback to update prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
-
 import Form from '@components/Form'
 
 const EditPrompt = () => {
@@ -18,17 +17,18 @@ const EditPrompt = () => {
 
 	useEffect(() => {
 		const getPromptDetails = async () => {
-			const response = await fetch(`/api/prompt/${promptId}`)
+			if (promptId) {
+				const response = await fetch(`/api/prompt/${promptId}`)
+				const data = await response.json()
 
-			const data = await response.json()
-
-			setPost({
-				prompt: data.prompt,
-				tag: data.tag,
-			})
+				setPost({
+					prompt: data.prompt,
+					tag: data.tag,
+				})
+			}
 		}
 
-		if (promptId) getPromptDetails()
+		getPromptDetails()
 	}, [promptId])
 
 	const updatePrompt = async (e) => {
@@ -36,14 +36,19 @@ const EditPrompt = () => {
 		setSubmitting(true)
 
 		if (!promptId) return alert('Prompt ID not found')
+
 		try {
 			const response = await fetch(`/api/prompt/${promptId}`, {
 				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json',
+				},
 				body: JSON.stringify({
 					prompt: post.prompt,
 					tag: post.tag,
 				}),
 			})
+
 			if (response.ok) {
 				router.push('/')
 			}
@@ -53,14 +58,17 @@ const EditPrompt = () => {
 			setSubmitting(false)
 		}
 	}
+
 	return (
-		<Form
-			type='Edit'
-			post={post}
-			setPost={setPost}
-			submitting={submitting}
-			handleSubmit={updatePrompt}
-		/>
+		<Suspense fallback={<div>Loading...</div>}>
+			<Form
+				type='Edit'
+				post={post}
+				setPost={setPost}
+				submitting={submitting}
+				handleSubmit={updatePrompt}
+			/>
+		</Suspense>
 	)
 }
 
